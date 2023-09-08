a9c63737f993cc79bd7cf05622fb8db49fc525dc
Edwin Marshall <edwin.marshall@fullstackacademy.com>
2023-09-07 19:25:04 -0700

    first commit

---
+++ combination.js
@@ -0,0 +1,10 @@
+/* fibs 
+1. Starting with the sequence 1, 1
+2. the next number in the sequence is the two previous numbers added together
+3. A function, fibs, takes a number n, and gets nth fibonacci nubmer
+*/
+
+
+
+
+console.log(Array(5).fill(null).map((_, i) => i))
